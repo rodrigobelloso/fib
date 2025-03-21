a04e4c5873d228dd41d2f7ea0ac1e05c64e948ad
Rodrigo Belloso <114697724+rodrigobelloso@users.noreply.github.com>
2025-03-21 17:26:50 +0100

    Mejorar la legibilidad de las funciones de multiplicación y potencia de matrices en fib.h

--- fib.h
+++ fib.h
@@ -10,10 +10,13 @@ void calculate_fibonacci_iterative(mpz_t result, long n, int verbose);
 void calculate_fibonacci_recursive(mpz_t result, long n, mpz_t *memo, int verbose);
 void calculate_fibonacci_matrix(mpz_t result, long n, int verbose);
 
-void matrix_multiply(mpz_t a11, mpz_t a12, mpz_t a21, mpz_t a22, mpz_t b11, mpz_t b12, 
-                    mpz_t b21, mpz_t b22, mpz_t c11, mpz_t c12, mpz_t c21, mpz_t c22);
-void matrix_power(mpz_t a11, mpz_t a12, mpz_t a21, mpz_t a22, long n, mpz_t result11,
-                 mpz_t result12, mpz_t result21, mpz_t result22, int verbose);
+void matrix_multiply(mpz_t a11, mpz_t a12, mpz_t a21, mpz_t a22, 
+                     mpz_t b11, mpz_t b12, mpz_t b21, mpz_t b22, 
+                     mpz_t c11, mpz_t c12, mpz_t c21, mpz_t c22);
+void matrix_power(mpz_t a11, mpz_t a12, mpz_t a21, mpz_t a22, 
+                  long n, 
+                  mpz_t result11, mpz_t result12, mpz_t result21, mpz_t result22, 
+                  int verbose);
 
 void display_help(const char *program_name);
 char *get_formatted_result(mpz_t result, OutputFormat format, int verbose);
